a70328e57c31fbf028f9bf03d316f63f900d86f9
lqtoan <lqtoan37@gmail.com>
2021-12-30 09:08:29 +0700

    update

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -8,40 +8,40 @@ const User = require('../models/user.model');
 // @route POST api/auth/register
 // @description Register a user
 // @access public
-router.post('/register', async (req, res) => {
-  const { username, password } = req.body;
+// router.post('/register', async (req, res) => {
+//   const { username, password } = req.body;
 
-  // Simple validation
-  if (!username || !password)
-    return res
-      .status(400)
-      .json({ success: false, message: 'Invalid username or password' });
+//   // Simple validation
+//   if (!username || !password)
+//     return res
+//       .status(400)
+//       .json({ success: false, message: 'Invalid username or password' });
 
-  try {
-    // Check for existing user
-    const user = await User.findOne({ username });
-    if (user)
-      res.status(400).json({ success: false, message: 'User already exists' });
-    // All good
-    const hashedPassword = await argon2.hash(password);
-    const newUser = new User({ username, password: hashedPassword });
-    await newUser.save();
+//   try {
+//     // Check for existing user
+//     const user = await User.findOne({ username });
+//     if (user)
+//       res.status(400).json({ success: false, message: 'User already exists' });
+//     // All good
+//     const hashedPassword = await argon2.hash(password);
+//     const newUser = new User({ username, password: hashedPassword });
+//     await newUser.save();
 
-    // Return token
-    const accessToken = jwt.sign(
-      { userId: newUser._id },
-      process.env.ACCESS_TOKEN_SECRET
-    );
-    res.json({
-      success: true,
-      message: 'User saved successfully',
-      accessToken,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ success: false, message: 'Internal server error' });
-  }
-});
+//     // Return token
+//     const accessToken = jwt.sign(
+//       { userId: newUser._id },
+//       process.env.ACCESS_TOKEN_SECRET
+//     );
+//     res.json({
+//       success: true,
+//       message: 'User saved successfully',
+//       accessToken,
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ success: false, message: 'Internal server error' });
+//   }
+// });
 
 // @route POST api/auth/login
 // @description Login a user
