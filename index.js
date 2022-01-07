0c359ededd5ff2039787193ba16fb7a1e41bfbee
lqtoan <lqtoan37@gmail.com>
2022-01-07 14:20:04 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config(); //
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
 
 const authRouter = require('./routes/auth.route');
 const postRouter = require('./routes/post.route');
@@ -28,7 +28,7 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World'));
 
